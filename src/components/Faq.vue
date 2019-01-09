f9f9b6f8db6b508d93ef29d704583a6b2197d99a
Olaf0703 <yamadah0703@gmail.com>
2019-01-09 15:34:43 +0100

    add axios module and edit Faq.vue

--- src/components/Faq.vue
+++ src/components/Faq.vue
@@ -1,12 +1,50 @@
 <template>
-  <div>faq</div>
+  <div class="faq">
+    <div class="container">
+      <section class="section">
+        <h1 class="title">FAQ</h1>
+        <h2 class="subtitle is-4">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h2>
+        <div class="columns" v-if="faqs && faqs.length">
+          <div class="column is-4" v-for="faq of faqs" :key="faq">
+            <div class="card">
+              <div class="card-content">
+                <p class="title">{{ faq.title }}</p>
+                <p class="answer">{{ faq.body }}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
-  name: "Faq"
+  name: "Faq",
+  data: () => ({
+    faqs: [],
+    errors: []
+  }),
+  created() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then(res => {
+        this.faqs = res.data.slice(0, 10);
+      })
+      .catch(e => {
+        this.errors.push(e);
+      });
+  }
 };
 </script>
 
+
+
 <style lang="scss" scoped>
+.columns {
+  flex-wrap: wrap;
+}
 </style>
